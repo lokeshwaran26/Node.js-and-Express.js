4ae462303db8e8130e0d06dd52188bcc29258c0f
lokesh <lokesh95664@gmail.com>
2023-04-11 21:20:43 +0530

    ..

--- 1-node-tutorial/app.js
+++ 1-node-tutorial/app.js
@@ -3,7 +3,8 @@ const { readFileSync, writeFileSync} = require('fs');
 
 const first = readFileSync('./content/subfolder/first.txt', 'utf-8');
 const second = readFileSync('./content/subfolder/second.txt', 'utf-8');
-console.log(first,  second);
+console.log(first);
+console.log(second);
 
 writeFileSync(
     'content/subfolder/result.txt',
