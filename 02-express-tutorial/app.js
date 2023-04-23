4fa8891862860a31f9a56730df64bd3324320e3c
lokesh <lokesh95664@gmail.com>
2023-04-23 19:56:26 +0530

    ...

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,51 +1,20 @@
-const http = require('http');
-const { readFileSync} = require('fs');
-
-const homePage = readFileSync('./navbar-app/index.html');
-const homeStyles = readFileSync('./navbar-app/styles.css');
-const homeLogo = readFileSync('./navbar-app/logo.svg');
-const homeLogic = readFileSync('./navbar-app/browser-app.js');
-const server = http.createServer((req,res)=>{
-
-    const url = req.url;
-
-    if ( url === '/'){
-    res.writeHead(200,{ 'content-type' : 'text/html'});
-    res.write(homePage);
-    res.end();
-    }
-
-    else if ( url === '/styles.css'){
-    res.writeHead(200,{ 'content-type' : 'text/css'});
-    res.write(homeStyles);
-    res.end();
-    }
-
-    else if ( url === '/logo.svg'){
-    res.writeHead(200,{ 'content-type' : 'image/svg+xml'});
-    res.write(homeLogo);
-    res.end();
-    }
-
-    else if ( url === '/browser-app.js'){
-    res.writeHead(200,{ 'content-type' : 'text/javascript'});
-    res.write(homeLogic);
-    res.end();
-    }
-
-    else if ( url === '/about'){
-    res.writeHead(200,{ 'content-type' : 'text/html'});
-    res.write('<h1> About Page </h1>');
-    res.end();
-    }
-
-    else {
-    res.writeHead(400,{ 'content-type' : 'text/html'});
-    res.write('<h1> Page not found</h1>');
-    res.end();
-    }
+const exp = require('constants');
+const express = require('express');
+const path = require('path');
 
 
+const app = express();
+
+app.use(express.static('./public'))
+
+// app.get('/',(req,res)=>{
+//     res.sendFile(path.resolve(__dirname,"./navbar-app/index.html"));
+// })
+
+app.all('*', (req, res)=>{
+    res.status(404).send(" Resource not found!");
 })
 
-server.listen(5000)
+app.listen(5000, ()=>{
+    console.log("server listening on port 5000...")
+})
